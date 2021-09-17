39d19578ab80b003e4cb2cea501f077b1c3c91e9
EricJonsson <32642935+EricJonsson@users.noreply.github.com>
2021-09-17 15:25:24 +0200

    Update server.py

--- src/server.py
+++ src/server.py
@@ -1,1 +1,9 @@
 # Communication Platform - Server
+
+# Object Created by developers to start a server that listen for clients to connect
+class CommunicationServer: # External
+  Clients = []
+  MaxConcurrentClients = 1
+  
+class ClientConnectionInfo:
+  port = 1
