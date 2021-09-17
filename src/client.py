25c0a7f627f87f7365804c95dd2e740654c00d93
EricJonsson <32642935+EricJonsson@users.noreply.github.com>
2021-09-17 15:36:23 +0200

    Update client.py

--- src/client.py
+++ src/client.py
@@ -1,1 +1,13 @@
 # Communication Platform - Client
+  
+# Method for sending a file to your opponent during a game.
+class Player:
+  _socket = None
+  def __init__(self, socket):
+    pass
+
+  def SendInformationToOpponent(self, information) # External
+    return 0
+
+  def ConnectToServer(self, port):
+    pass
